8f42f081a8a095e9546fd0de5eec98876af04b3e
cainan <33745031+pactera-cart-cainan@users.noreply.github.com>
2020-04-24 19:38:34 +0900

    Add files via upload

--- graph-tutorial-for-device/routes/auth.js
+++ graph-tutorial-for-device/routes/auth.js
@@ -1,46 +1,72 @@
 var express = require('express');
-var passport = require('passport');
 var router = express.Router();
 var adal = require('adal-node');
 var MemoryCache = require('../lib/memory-cache');
-
+var crypto = require('crypto');
+var sampleParameters;
+sampleParameters = {
+  tenant: '3c33e967-6bae-43fb-80f8-83d093bd41ed',
+  authorityHostUrl: 'https://login.windows.net',
+  clientId: '2a07af9e-278f-4969-b696-70e1e643ebc2',
+  clientSecret: 'r]/jpKb2T0r:u70w-g]-1ff-Q7-pE.W?',
+  redirectUri: 'http://localhost:3000/auth/getAToken',
+  resource: "00000002-0000-0000-c000-000000000000" //URI of resource where token is valid
+};
+var AuthenticationContext = require('adal-node').AuthenticationContext;
+var templateAuthzUrl = 'https://login.windows.net/' +
+  sampleParameters.tenant +
+  '/oauth2/authorize?response_type=code&client_id=' +
+  sampleParameters.clientId +
+  '&redirect_uri=' +
+  sampleParameters.redirectUri +
+  '&state=<state>&resource=' +
+  sampleParameters.resource;
+function createAuthorizationUrl(state) {
+  return templateAuthzUrl.replace('<state>', state);
+}
 /* GET auth callback. */
-// router.get('/signin',
-//   function  (req, res, next) {
-//     passport.authenticate('azuread-openidconnect',
-//       {
-//         response: res,
-//         prompt: 'login',
-//         failureRedirect: '/',
-//         failureFlash: true,
-//         successRedirect: '/'
-//       }
-//     )(req,res,next);
-//   }
-// );
+router.get('/signin',
+  function (req, res, next) {
+    crypto.randomBytes(48, function(ex, buf) {
+      var token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+  
+      res.cookie('authstate', token);
+      var authorizationUrl = createAuthorizationUrl(token);
+  
+      res.redirect(authorizationUrl);
+    });
+});
 
-// router.post('/callback',
-//   function(req, res, next) {
-//     passport.authenticate('azuread-openidconnect',
-//       {
-//         response: res,
-//         failureRedirect: '/',
-//         failureFlash: true,
-//         successRedirect: '/'
-//       }
-//     )(req,res,next);
-//   },
-//   function(req, res) {
-//     // TEMPORARY!
-//     // Flash the access token for testing purposes
-//     req.flash('error_msg', {message: 'Access token', debug: req.user.accessToken});
-//     res.redirect('/');
-//   }
-// );
+router.get('/getAToken', function(req, res) {
+  if (req.cookies.authstate !== req.query.state) {
+    res.send('error: state does not match');
+  }
+  var authorityUrl = sampleParameters.authorityHostUrl + '/' + sampleParameters.tenant;
+  var authenticationContext = new AuthenticationContext(authorityUrl);
+
+  authenticationContext.acquireTokenWithAuthorizationCode(
+    req.query.code,
+    sampleParameters.redirectUri,
+    sampleParameters.resource,
+    sampleParameters.clientId, 
+    sampleParameters.clientSecret,
+    function(err, response) {
+      var errorMessage = '';
+      if (err) {
+        errorMessage = 'error: ' + err.message + '\n';
+      } else {
+        errorMessage += 'response: ' + JSON.stringify(response);
+        console.log(errorMessage);
+        res.cookie('accessToken', response.accessToken);
+        res.redirect('/');
+      }
+    }
+  );
+});
 
 router.get('/signout',
-  function(req, res) {
-    req.session.destroy(function(err) {
+  function (req, res) {
+    req.session.destroy(function (err) {
       res.clearCookie("accessToken");
       res.redirect('/');
     });
